1d6946c419a8455ce3ffc79583a6160efda9ea6d
NikitaK <kuranovnikita@gmail.com>
2024-02-23 21:27:55 +0400

    'start_commit'

---
+++ json_app/json_app.go
@@ -0,0 +1,32 @@
+package json_app
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+func RespondWithError(w http.ResponseWriter, code int, msg string) {
+	if code > 499 {
+		log.Println("Respondong with 5xx error", msg)
+	}
+	type errResponse struct {
+		Error string `json:"error"`
+	}
+
+	RespondWithJSON(w, code, errResponse{
+		Error: msg,
+	})
+}
+
+func RespondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	dat, err := json.Marshal(payload)
+	if err != nil {
+		log.Printf("Failed to marshal JSON response: %v\n", payload)
+		w.WriteHeader(500)
+		return
+	}
+	w.Header().Add("Content-Type", "applictaion/json")
+	w.WriteHeader(code)
+	w.Write(dat)
+}
